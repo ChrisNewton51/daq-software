16cc8b83984e8c2525ab913f2964591a1ee8bb7c
ChrisNewton51 <internet.intern1@gmail.com>
2022-04-14 15:03:49 -0400

    Final

---
+++ Final_Application/resources/app/DAQ_Unit_Arduino_Code/DAQ_Unit_Arduino_Code.ino
@@ -0,0 +1,242 @@
+//Included Libraries
+#include <SPI.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+//Voltage Declarations
+int VoltageValue = 0; //variable used to read from the sensor
+float VoltageReading; //variable used to send to the software
+float R1 = 30000.0; //value of resistor 1
+float R2 = 7500.0; //value of resistor 2
+
+//Current Declarations
+unsigned int x=0;
+float AcsValue,Samples,AvgAcs,AcsValueF;
+
+//Capacitance Declarations
+int analogPin = 1;
+int chargePin = 12; //changed this ------------------------------------------------------------- should be 12
+int dischargePin = 11;
+int R3 = 10000;
+
+unsigned long startTime; 
+unsigned long elapsedTime;
+
+float microFarads;                
+float nanoFarads;
+
+//Inductance Declarations
+double pulse, frequency, capacitance, inductance;
+
+//Magnetic Field Declarations
+int rawvalue;
+float Gval;
+float Tval;
+
+//LCD Declarations
+LiquidCrystal_I2C lcd(0x27,20,4); //LCD setup of 20 chars by 4 lines
+
+//Button Declarations
+const int buttonPin = 7;
+int Button_State = 0;
+int button_value = 1;
+
+void setup() {
+  //Setting up capacitance
+  pinMode(chargePin, OUTPUT);     
+  digitalWrite(chargePin, LOW); 
+  //Setting up inductance
+  pinMode(6, INPUT); //Input from the comparator output
+  pinMode(5, OUTPUT);//output through a 150 ohm resistor to thr LC circuit
+  //Button Setup
+  pinMode(buttonPin,INPUT);
+  //setting up the LCD screen
+  Serial.begin(115200);
+  lcd.init();
+  lcd.backlight();
+
+}
+
+void loop() {
+  delay(150);
+  Button_State = digitalRead(buttonPin); //read the value of the button
+  
+  if(Button_State == HIGH) //if the button is pushed
+  {
+    Serial.println("switch");
+    if(button_value == 5) //if the current value of the button is 5 then reset to 1
+    {
+      lcd.clear();
+      button_value = 1; //reset the button value to 1
+      delay(150);
+    }
+    else //if the button value is 1-4
+    {
+      lcd.clear();
+      button_value = button_value +1; //increment the value of the button by 1
+      delay(150);
+    }
+  }
+  
+  switch(button_value) //swicth determines which sensor is selected
+  {
+    case 1: //voltage case
+        VoltageValue = analogRead(A0);
+        VoltageReading = VoltageValue * (4.59/1024)*((R1 + R2)/R2);
+        Serial.print("1 "); //display the mode code to the software
+        Serial.println(VoltageReading); //sending the value of the sensor reading
+        //delay(500);
+        
+        //Display on the LCD
+        lcd.setCursor(0,0); // set cursor to 1 symbol of 1 line
+        lcd.print("Voltage");
+        lcd.setCursor(0,1); // set cursor to 1 symbol of 2 line
+        //lcd.print("Voltage: ");
+        lcd.print(VoltageReading); 
+        lcd.print(" V  ");
+        break;
+    case 2: //current case
+        AcsValue=0.0,Samples=0.0,AvgAcs=0.0,AcsValueF=0.0; //reseting to 0.0
+
+        for (int x = 0; x < 150; x++)
+        { //Get 150 samples
+        AcsValue = analogRead(A3);     //Read current sensor values
+        Samples = Samples + AcsValue;  //Add samples together
+        //delay (3); // let ADC settle before next sample 3ms
+        }
+
+        AvgAcs=Samples/150.0;//Taking Average of Samples
+        //AcsValueF = 512 - (AvgAcs*0.0265);
+        AcsValueF = -((AvgAcs * (5.0 / 1024.0) - 2.5)*-5.5);
+        Serial.print("2 "); //display the mode code to the software
+        Serial.println(AcsValueF);//Print the read current on Serial monitor
+        //delay(50);
+
+        //Display on the LCD
+        lcd.setCursor(0,0); // set cursor to 1 symbol of 1 line
+        lcd.print("Current");
+        lcd.setCursor(1,1); // set cursor to 1 symbol of 2 line
+        //lcd.print(AvgAcs); 
+        lcd.print(AcsValueF); 
+        lcd.print(" A      ");
+
+        delay(50);
+        break;
+    case 3: //capacitance case
+      digitalWrite(chargePin, HIGH); // Begins charging the capacitor
+      startTime = millis(); // Begins the timer
+      
+      while(analogRead(analogPin) < 648)
+      {       
+        // Does nothing until capacitor reaches 63.2% of total voltage
+      }
+    
+      elapsedTime= millis() - startTime; // Determines how much time it took to charge capacitor
+      microFarads = ((float)elapsedTime / R3) * 1000;
+      //Serial.print(elapsedTime);       
+      //Serial.print(" mS    ");
+      lcd.setCursor(0,0); // set cursor to 1 symbol of 1 line
+      lcd.print("Capacitance"); 
+     
+      //lcd.print(elapsedTime);
+      //lcd.print(" mS   "); 
+             
+    
+      if (microFarads >= 1) // Determines if units should be micro or nano and prints accordingly
+      {
+        Serial.print("3 ");
+        Serial.print((long)microFarads);       
+        Serial.println(" uF"); 
+        lcd.setCursor(0,1); // set cursor to 1 symbol of 2 line 
+        lcd.print ((long)microFarads);
+        lcd.print (" microFarads  ");       
+      }
+    
+      else
+      {
+        Serial.print("3 ");
+        nanoFarads = microFarads * 1000.0;      
+        Serial.print((long)nanoFarads);         
+        Serial.println(" nF");
+        lcd.setCursor(0,1); // set cursor to 1 symbol of 2 line 
+        lcd.print ((long)nanoFarads);
+        lcd.print (" nanoFarads  ");           
+        //delay(500); 
+      }
+    
+      digitalWrite(chargePin, LOW); // Stops charging capacitor
+      pinMode(dischargePin, OUTPUT); 
+      digitalWrite(dischargePin, LOW); // Allows capacitor to discharge  
+        
+      while(analogRead(analogPin) > 0)
+      {
+        // Do nothing until capacitor is discharged      
+      }
+    
+      pinMode(dischargePin, INPUT); // Prevents capacitor from discharging 
+
+      delay(50);
+      break;
+    case 4: //inductance case
+      digitalWrite(5, HIGH);
+      delay(5);//give some time to charge inductor.
+      digitalWrite(5,LOW);
+      delayMicroseconds(100); //make sure resination is measured
+      pulse = pulseIn(6,HIGH,5000);//returns 0 if timeout
+      
+      lcd.setCursor(0,0); 
+      lcd.print("Inductance:");
+
+      if(pulse > 0.1){ //if a timeout did not occur and it took a reading:
+        
+        
+      //  #error insert your used capacitance value here. Currently using 2uF. Delete this line after that
+        capacitance = 2.08E-6; // - insert value here
+        
+        frequency = 1.E6/(2*pulse);
+        inductance = 1./(capacitance*frequency*frequency*4.*3.14159*3.14159);//one of my profs told me just do squares like this
+        inductance *= 1E3; //note that this is the same as saying inductance = inductance*1E6
+        
+        Serial.print("4 ");
+        Serial.println( inductance );
+        delay(10);
+      
+        //LCD print
+        lcd.setCursor(0,1); 
+        lcd.print(inductance);
+        lcd.print(" mH    ");
+        //lcd.setCursor(14,1); 
+        //lcd.print("uH");          
+        delay(10);
+      } else {
+        Serial.println("4 ");
+      }
+
+      
+      break;
+    case 5: //magnetic field case    
+      lcd.setCursor(0, 0);
+      lcd.print("Magnetic Field");
+      rawvalue = analogRead(A2);
+
+      Gval = 436.65 - (rawvalue*2.13);
+      Tval = Gval/1000;
+
+      Serial.print("5 ");
+      Serial.println(Tval);
+      lcd.setCursor(0,1);
+      lcd.print(Gval);
+      lcd.print(" Gauss       ");
+      lcd.setCursor(0,2);
+      lcd.print(Tval);
+      lcd.print(" mT      ");
+
+      delay(50);
+      break;
+    default: 
+      Serial.print("1 ");
+      Serial.println(random(0.00,1.00));
+      break;
+  }
+  
+}
---
+++ Final_Application/resources/app/renderer.js
@@ -0,0 +1,161 @@
+// electron-packager ./ ics --platform=win32 --arch=x64 --electron-version=8.1.1
+
+
+// Require Statements
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+const { ipcRenderer } = require('electron');
+const remote = require('electron').remote;
+const dialog = remote.require('electron').dialog;
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+var recordData = false;
+(function($) {
+    $(document).ready(function() {        
+        SerialPort.list().then(function(ports){
+            $("#select-port").append(`<option>${ports[0].path}</option>`)
+        });
+
+        $("#submit-select").on('click', function() {
+            if ($("#select-port option:selected").text() != "Select Port") {
+                $("#port-container").hide();
+                //global.port.com = $("#select-port option:selected").text();
+                
+                // Set MyGlobalVariable.
+                ipcRenderer.send( "setPort",  $("#select-port option:selected").text());
+                console.log(remote.getGlobal( "port" ));
+                
+                // Variable Definitions
+                const port = new SerialPort({
+                    path: remote.getGlobal( "port" ),
+                    baudRate:115200,
+                    parser: new ReadlineParser("\n")
+                });         
+
+                ipcRenderer.on('open-port', (event, arg) => {
+                    port.open();
+                });
+
+                const parser = port.pipe(new ReadlineParser({ delimiter: '\n' }));// Read the port data
+                var selector, date, newDate, path;
+                var recordData = false;
+                var i = 0;
+                var index = 0;
+                var time = [];
+                var values = [];
+                var record = [];
+                
+                $("#popout").on('click', function() {
+                    port.close(function (err) {
+                        console.log('port closed', err);
+                    });
+                })
+
+                // Open Serial Port
+                port.on("open", () => {
+                    console.log('serial port open');
+                });
+
+                // Read data realtime
+                parser.on('data', data =>{
+                    selector = data.substring(0,1);
+                    date = new Date();
+                    newDate = date.toString().slice(4,25);
+                    
+                    if (data.trim() == "switch") {
+                        $("#voltval").text("0.00 V");
+                        $("#currentval").text("0.00 A");
+                        $("#capval").text("0.00 nF");
+                        $("#indval").text("0.00 mT");
+                        $("#magval").text("0.00 mH");
+                    }
+
+                    switch (Number(selector)) {
+                        case 1:
+                            $("#voltval").text(data.substring(1) + "V");
+                            break;
+                        case 2:
+                            $("#currentval").text(data.substring(1) + "A");
+                            break;
+                        case 3:
+                            $("#capval").text(data.substring(1));
+                            break;
+                        case 4:
+                            $("#indval").text(data.substring(1) + "mH");
+                            break;
+                        case 5:
+                            $("#magval").text(data.substring(1) + "mT");
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (recordData == true) {
+                        time[index] = newDate;
+                        values[index] = data.substring(1);
+                        record[index] = {
+                            time: time[index],
+                            value: values[index]
+                        };
+                        $("#data").text(values.length + " Data Points");
+                        index++;
+                    }
+                    
+                    
+
+                    i++;
+                });
+                $("#play").on('click', function() {
+                    let options = {
+                        // See place holder 1 in above image
+                        title : "Select File Location", 
+        
+                        properties: ['openDirectory']
+                    }
+                    var newPath;
+        
+                    dialog.showOpenDialog(options)
+                        .then(filePaths => {
+                            path = String(filePaths.filePaths)
+                            newPath = path.replace("undefined", "")
+                            newPath = newPath + "\\data.csv";
+                            const csvWriter = createCsvWriter({
+                                path: newPath,
+                                header: [
+                                    {id: 'time', title: 'Time'},
+                                    {id: 'value', title: 'Value'},
+                                ]
+                            });
+                    
+                            csvWriter
+                                .writeRecords(record)
+                                .then(()=> console.log('The CSV file was written successfully'));
+        
+                            $("#notice").text("File has been downloaded at " + newPath);
+                        })
+                    
+                });
+                $("#record").on('click', function() {
+                    if (recordData == false) {
+                        recordData = true;
+                        $("#record").text("Stop Recording");
+                    } else {
+                        recordData = false;
+                        $("#record").text("Record Data");
+                    }
+                });
+                $("#restart").on('click', function() {
+                    time = [];
+                    values = [];
+                    record = [];
+                    index = 0;
+                    $("#data").text(values.length + " Data Points");
+                    $("#notice").hide();
+                })
+            }
+        })   
+            
+        
+        
+
+    });
+})( jQuery );
